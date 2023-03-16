039fc4e9249ca9e65596b0de0389c89a5ded94ae
Bolero-fk <fuku_0000@outlook.jp>
2023-03-16 23:02:01 +0900

    処理をクラス化した

---
+++ account.py
@@ -0,0 +1,52 @@
+import os
+import json
+import getpass
+
+class Account:
+    ACCOUNT_PATH = "private/account.json"
+
+    EMAIL = ""
+    PASSWARD = ""
+
+    def __init__(self, site_name):
+        self.EMAIL, self.PASSWARD = self.get_account(site_name)
+
+    def save_login_info(self, site_name):
+        # ユーザーからの入力を受け付ける
+        email = input("メールアドレスを入力してください: ")
+        password =  getpass.getpass("パスワードを入力してください: ")
+
+        # 入力されたデータを辞書に格納
+        login_info = {
+            site_name: {
+            "email": email,
+            "password": password
+            }
+        }
+
+        # データをJSONファイルに保存
+        with open(self.ACCOUNT_PATH, "w") as f:
+            json.dump(login_info, f)
+
+    def load_account_from_json(self, site_name):
+        # ファイルからJSONデータを読み込み
+        with open(self.ACCOUNT_PATH, 'r') as f:
+            login_info = json.load(f)
+
+        # メールアドレスとパスワードを取得
+        email = login_info[site_name]['email']
+        password = login_info[site_name]['password']
+
+        return email, password
+
+    def get_account(self, site_name):
+
+        if not os.path.exists(self.ACCOUNT_PATH):
+            self.save_login_info(site_name)
+        
+        try:
+            return self.load_account_from_json(site_name)
+        except:
+            self.save_login_info(site_name)
+            return self.load_account_from_json(site_name)
+
---
+++ paiza_scaut_crawler.py
@@ -0,0 +1,91 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from account import Account
+
+class PaizaScautCrawler:
+    LOGIN_URL = "https://paiza.jp/sign_in"
+    MESSAGE_URL = "https://paiza.jp/messages"
+
+    def __init__(self):
+        pass
+
+    def get_scauts(self):
+        browser = self.initialize_browser()
+        try:
+            self.login(browser)
+            scauts = self.fetch_scauts(browser)
+        finally:
+            browser.quit()
+
+        return scauts
+
+    def initialize_browser(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('headless')
+        new_driver = ChromeDriverManager().install()
+        service = Service(executable_path=new_driver)
+        browser = webdriver.Chrome(service=service, options=options)
+
+        return browser
+
+    def login(self, browser):
+        paize_account =  Account("paiza")
+
+        # URLを開く
+        browser.implicitly_wait(10)
+
+        browser.get(self.LOGIN_URL)
+
+        # ユーザー名を入力
+        browser.find_element('css selector', 'input[type="email"]').send_keys(paize_account.EMAIL)
+
+        # パスワードを入力
+        browser.find_element('css selector', 'input[type="password"]').send_keys(paize_account.PASSWARD)
+
+        # ログインボタンをクリック
+        browser.find_element('css selector', 'input[type="submit"]').click()
+
+        time.sleep(2)
+    
+    def fetch_scauts(self, browser):
+
+        browser.get(self.MESSAGE_URL)
+        time.sleep(2)
+
+        # スクロール可能な要素を取得する
+        scrollable_form = browser.find_element(By.CLASS_NAME, "ScoutMessagesScrollableFrame")
+
+        # スクロール量を設定する
+        scroll_amount = 1000
+
+        # スカウトメッセージの数
+        sum_message_num = int(browser.find_element(By.CLASS_NAME, "ScoutMessagesCountNum").text[:-1])
+
+        scaut_cards = scrollable_form.find_elements(By.CLASS_NAME, "MessageCard")
+
+        loop_count = 0
+        loop_max = 1000
+
+        # スクロール可能な要素を最下部までスクロールする
+        while len(scaut_cards) != sum_message_num and loop_count < loop_max:
+            
+            # スクロール可能な要素の現在位置を取得する  
+            current_position = browser.execute_script('return arguments[0].scrollTop', scrollable_form)
+            
+            # スクロール可能な要素を指定量だけスクロールする
+            browser.execute_script(f'arguments[0].scrollTop = {current_position + scroll_amount}', scrollable_form)
+            
+            # スクロールした後に読み込みが完了するまで少し待つ
+            time.sleep(1)
+
+            scaut_cards = scrollable_form.find_elements(By.CLASS_NAME, "MessageCard")
+
+            loop_count += 1
+
+        return [card.get_attribute('innerHTML') for card in scaut_cards]
