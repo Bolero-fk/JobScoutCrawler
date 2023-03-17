051852d94045fdbecc5f8dd1f9e0d5ebcf1f78d7
Bolero-fk <fuku_0000@outlook.jp>
2023-03-17 22:42:37 +0900

    複数サイトのアカウントが必要な場合に対応した

--- account.py
+++ account.py
@@ -10,12 +10,17 @@ class Account:
 
     def __init__(self, site_name):
         self.EMAIL, self.PASSWARD = self.get_account(site_name)
-
-    def save_login_info(self, site_name):
+    
+    def get_login_info(self):
         # ユーザーからの入力を受け付ける
         email = input("メールアドレスを入力してください: ")
         password =  getpass.getpass("パスワードを入力してください: ")
 
+        return email, password
+
+    def save_login_info(self, site_name, append_mode = True):
+        email, password = self.get_login_info()
+        
         # 入力されたデータを辞書に格納
         login_info = {
             site_name: {
@@ -23,6 +28,10 @@ class Account:
             "password": password
             }
         }
+
+        if(append_mode):
+            with open(self.ACCOUNT_PATH, 'r') as f:
+                login_info.update(json.load(f)) 
 
         # データをJSONファイルに保存
         with open(self.ACCOUNT_PATH, "w") as f:
@@ -42,11 +51,11 @@ class Account:
     def get_account(self, site_name):
 
         if not os.path.exists(self.ACCOUNT_PATH):
-            self.save_login_info(site_name)
+            self.save_login_info(site_name, False)
         
         try:
             return self.load_account_from_json(site_name)
         except:
-            self.save_login_info(site_name)
+            self.save_login_info(site_name, True)
             return self.load_account_from_json(site_name)
 
